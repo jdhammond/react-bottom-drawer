741d77d4961e01f737ff8decc435513c7fd8ad03
Jonah DH <jdh@Jonahs-MacBook-Air.local>
2024-04-15 08:49:12 -0400

    add useIsomorphicLayoutEffect for server-side rendering

--- src/lib/hooks/usePreventScroll.ts
+++ src/lib/hooks/usePreventScroll.ts
@@ -1,20 +1,22 @@
-import * as React from "react";
-import {disableBodyScroll, enableBodyScroll} from "body-scroll-lock";
+import * as React from 'react';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 function usePreventScroll(enabled: boolean, contentWrapperClass: string) {
-  React.useLayoutEffect(() => {
-    if (typeof document === "undefined" || !enabled) {
+  const useIsomorphicLayoutEffect =
+    typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+  useIsomorphicLayoutEffect(() => {
+    if (typeof document === 'undefined' || !enabled) {
       return;
     }
-    
+
     const scrollableElement = document.querySelector(`.${contentWrapperClass}`);
 
     disableBodyScroll(scrollableElement);
-    console.log("Disable body scrol");
+    console.log('Disable body scrol');
 
     return () => {
       enableBodyScroll(scrollableElement);
-    }
+    };
   }, [enabled]);
 }
 
